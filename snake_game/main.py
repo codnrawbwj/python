2a4b1b5cfd0ca5061d9eec01e76beabbfc3629c9
Chae-uk <codnrawbwj@gmail.com>
2021-06-18 00:38:58 +0900

    half done

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
+
 
 screen = Screen()
 screen.title("You are now a snake!")
@@ -14,29 +16,19 @@ screen.bgcolor("black")
 #     new_seg.color("white")
 #     new_seg.goto(pos)
 
-turtle_list = []
+snake = Snake()
 
-for turtle_index in range(0, 3):
-    new_turtle = Turtle()
-    new_turtle.penup()
-    new_turtle.shape("square")
-    new_turtle.color("white")
-    new_turtle.goto(x=-20*turtle_index, y=0)
-    turtle_list.append(new_turtle)
-
-screen.update()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 
 is_game_on = True
 while is_game_on:
     screen.update()
-    time.sleep(.5)
+    time.sleep(.05)
 
-    # Moving a snake
-    for turtle_num in range(len(turtle_list)-1, 0, -1):
-        new_x = turtle_list[turtle_num - 1].xcor()
-        new_y = turtle_list[turtle_num - 1].ycor()
-        turtle_list[turtle_num].goto(new_x, new_y)
-    turtle_list[0].fd(20)
-    
+    snake.move()
 
 screen.exitonclick()
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,9 +1,16 @@
 from turtle import Turtle
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
-        turtle_list = []
+        self.turtle_list = []
+        self.set_up()
+        self.head = self.turtle_list[0]
 
     def set_up(self):
         for turtle_index in range(0, 3):
@@ -14,9 +21,25 @@ class Snake:
             new_turtle.goto(x=-20 * turtle_index, y=0)
             self.turtle_list.append(new_turtle)
 
-    def move(self, turtle_list):
-        for turtle_num in range(len(turtle_list) - 1, 0, -1):
-            new_x = turtle_list[turtle_num - 1].xcor()
-            new_y = turtle_list[turtle_num - 1].ycor()
-            turtle_list[turtle_num].goto(new_x, new_y)
-        turtle_list[0].fd(20)
+    def move(self):
+        for turtle_num in range(len(self.turtle_list) - 1, 0, -1):
+            new_x = self.turtle_list[turtle_num - 1].xcor()
+            new_y = self.turtle_list[turtle_num - 1].ycor()
+            self.turtle_list[turtle_num].goto(new_x, new_y)
+        self.head.fd(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
